a8d292f1cd38ee8059d8eeeba75ab414025a49a1
jamesa <jamesacs329@gmail.com>
2023-07-31 14:38:15 -0400

    complete project

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // Add your code to this file
+let body = document.querySelector("body");
+body.style.backgroundColor = "blue";
+
+imgs = document.querySelectorAll("img");
+for (let i = 0; i < imgs.length; i++) {
+	imgs[i].style.border = "10px dotted yellow";
+}
